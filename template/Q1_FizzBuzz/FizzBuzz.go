46a9caebea4b96b63d175ef3b026f42f0d2a78ff
2525 <hi5ro20mu1999@gmail.com>
2022-05-07 22:30:07 +0900

    FizzBuzz終了

--- template/Q1_FizzBuzz/FizzBuzz.go
+++ template/Q1_FizzBuzz/FizzBuzz.go
@@ -1,11 +1,24 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"strconv"
+)
 func main(){
     for i := 1; i <= 100; i++ {
         str := ""
 
         // ここから記述
-
+        
+        if (i % 15 == 0){
+          str = "FizzBuzz"
+        }else if (i % 5 == 0){
+          str = "Buzz"
+        }else if (i % 3 == 0){
+          str = "Fizz"
+        }else {
+          str = strconv.Itoa(i)
+        }
 
         // ここまで記述
 
